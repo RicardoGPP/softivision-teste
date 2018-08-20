77201f4bc6f7709d5588f2c294cd63fafcc92334
Ricardo Giovani <ricardo.piantavinha@gmail.com>
2018-08-20 12:16:32 -0300

    Adição do arquivo "index.php" para mensagem de boas-vindas

---
+++ Back-end/Rest/index.php
@@ -0,0 +1,19 @@
+<h4>Bem-vindo ao webservice do projeto solicitado na avaliação para desenvolvedor Front-End da SoftVision.</h4>
+Estes são os serviços oferecidos por este servidor:
+<ul>
+    <li><b>Lista de cargos:</b> /softvision/api/cargo/listar</li>
+    <li><b>Lista de empresas:</b> /softvision/api/empresa/listar</li>
+    <li><b>Buscar cargo de funcionario:</b> /softvision/api/funcionario-cargo/buscar/[codigo]</li>
+    <li><b>Listar cargos de funcionario:</b> /softvision/api/funcionario-cargo/listar</li>
+    <li><b>Incluir cargo de funcionario:</b> /softvision/api/funcionario-cargo/incluir (método POST)</li>
+    <li><b>Excluir cargo de funcionario:</b> /softvision/api/funcionario-cargo/excluir (método POST)</li>
+    <li><b>Editar cargo de funcionario:</b> /softvision/api/funcionario-cargo/editar (método POST)</li>
+</ul>
+Use a vontade!
+<br>
+<br>
+--
+<br>
+Ricardo Giovani Piantavinha Perandré
+<br>
+(19) 98229-7535
